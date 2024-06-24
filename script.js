15518f62518d1523625049244ad2dc4538444a89
milagros729 <milagrostorales443@gmail.com>
2024-06-24 17:29:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//PROGRAMA
+
+const texto= document.querySelector("#texto");
+const entrada= document.querySelector("#entrada");
+//<const boton= document.querySelector("#boton");>
+
+function saludar(nombre){  
+    texto.innerText="TE-AMO,"+nombre+"!";
+}
+
+boton.addEventListener("click",()=>saludar(entrada.value));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Hola mundo");
+let nombre="Milagros";
+let edad="19";
+console.log("Mi nombre es", nombre, "y tengo", edad,"años");
+nombre="Pedro"
+edad="95"
+console.log("Mi nombre es", nombre, "y tengo", edad,"años");
+const PI=3
+console.log(PI)
+let num1=5
+let num2=7
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+let nombre1="Juan"
+let nombre2="Derlis"
+console.log(nombre1+nombre2)
+console.log(5>7)
+console.log(10<15)
+console.log(10===10)
+console.log(5!==5)
+console.log(3==="3")
+console.log(5>=4)
+
+
+
